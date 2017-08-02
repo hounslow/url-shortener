8250cfac38273170ebde677496e8bf98d94a1f61
hounslow <hounslowmatthew@gmail.com>
2017-08-02 19:48:22 +0000

    Fixed update route

--- express_server.js
+++ express_server.js
@@ -49,7 +49,7 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls');
 });
 
-app.post("/urls/:id/update", (req, res) => {
+app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls');
 });
